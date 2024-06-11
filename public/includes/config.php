8d0c893c81f1dc59316770bf7928388260e21ec0
Shane Fernandes <shanesf5@gmail.com>
2024-06-11 21:28:18 +0100

    Add phpdotenv library to load environment variables from .env file

--- public/includes/config.php
+++ public/includes/config.php
@@ -1,11 +1,12 @@
 <?php
 function getDbConnection(){
-    $lines = file(__DIR__ . '\..\..\.env');
-    foreach ($lines as $line) {
-        $line = trim($line);
-        [$key, $value] = explode('=', $line);
-        $$key = $value;
-    }
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__."\..\..");
+    $dotenv->load();
+
+    $DB_HOST=$_ENV['DB_HOST'];
+    $DB_USER=$_ENV['DB_USER'];
+    $DB_PASS=$_ENV['DB_PASS'];
+    $DB_NAME=$_ENV['DB_NAME'];
 
     try {
         $conn = new PDO("mysql:host=" . $DB_HOST . ";dbname=" . $DB_NAME, $DB_USER, $DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
